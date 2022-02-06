334d08f61bbbaf387eb1d49a9ffdebd6be6b34b3
Kenneth Terrago <kterrago091893@gmail.com>
2022-02-06 14:51:10 +0800

    :construction: add BlocObserver

---
+++ lib/counter_observer.dart
@@ -0,0 +1,14 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter/foundation.dart';
+
+/// {@template counter_observer}
+/// [BlocObserver] for the counter application which
+/// observers all state changes.
+/// {@endtemplate}
+class CounterObserver extends BlocObserver {
+  @override
+  void onChange(BlocBase bloc, Change change) {
+    super.onChange(bloc, change);
+    debugPrint('${bloc.runtimeType} $change');
+  }
+}
